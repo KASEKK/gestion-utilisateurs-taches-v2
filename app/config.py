1a611180b656c0a60e670dc3744f60f048d52b11
kasekk <kosecki1994@gmail.com>
2025-05-21 15:52:17 +0200

    connexion db pgAdmin

--- app/config.py
+++ app/config.py
@@ -1,10 +1,10 @@
-# Paramètres de connexion
+# Parametres de connexion
 scheme         = "postgresql+psycopg2"
-username       = "votre_utilisateur"
-password       = "votre_mot_de_passe"
+username       = "postgres"
+password       = "skmSKM01"
 hostname       = "localhost"
 port           = "5432"
-database_name  = "nom_de_votre_base"
+database_name  = "gestion_utilisateurs"
 
 # Construction de l'URL de connexion
 URL_DB = f"{scheme}://{username}:{password}@{hostname}:{port}/{database_name}"
--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 # app/main.py
+# -*- coding: utf-8 -*-
+
 
 from sqlalchemy import create_engine
 from app.config import URL_DB
@@ -7,8 +9,7 @@ from app.models import Base
 def init_db():
     engine = create_engine(URL_DB)
     Base.metadata.create_all(engine)
-    print("✅ Base de données et tables créées avec succès.")
+    print("✅ Base de donnees et tables creees avec succes.")
 
 if __name__ == "__main__":
     init_db()
- commentaire
